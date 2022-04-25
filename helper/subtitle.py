ad8e1e5482901d7835b663276a59396ae584fef8
ShanHui <shanhui@agora.io>
2022-04-25 21:28:24 +0800

    optimize file encoding

--- helper/subtitle.py
+++ helper/subtitle.py
@@ -5,10 +5,6 @@ __author__ = "winking324@gmail.com"
 from charset_normalizer import from_path
 
 
-def file_encoding(filename):
-    return from_path(filename).best().encoding
-
-
 def filter_dialogue(dialogue):
     pos = dialogue.find('\\N')
     dialogue = dialogue[:pos] if pos > 0 else dialogue
@@ -27,32 +23,32 @@ def filter_dialogue(dialogue):
 
 def parse_subtitle(subtitle_file):
     dialogues = []
-    with open(subtitle_file, "r", encoding=file_encoding(subtitle_file)) as f:
-        dialogue_format = []
-        start_parse = False
-        for line in f:
-            if not start_parse:
-                if not line.startswith("[Events]"):
-                    continue
-                else:
-                    start_parse = True
+    dialogue_format = []
+    start_parse, text_pos, start_pos, end_pos = False, 0, 0, 0
+    subtitle_file_content = str(from_path(subtitle_file).best()).splitlines()
+    for line in subtitle_file_content:
+        if not start_parse:
+            if not line.startswith("[Events]"):
+                continue
+            else:
+                start_parse = True
 
-            if line.startswith("Dialogue:"):
-                if not dialogue_format:
-                    print("ERROR: ass unknown format")
-                    return
-                formated_line = line[9:].strip().split(',')
-                dialogue_text = filter_dialogue(formated_line[text_pos])
-                if dialogue_text:
-                    dialogues.append({
-                        "text": dialogue_text,
-                        "start": formated_line[start_pos],
-                        "end": formated_line[end_pos]
-                    })
+        if line.startswith("Dialogue:"):
+            if not dialogue_format:
+                print("ERROR: ass unknown format")
+                return
+            formated_line = line[9:].strip().split(',')
+            dialogue_text = filter_dialogue(formated_line[text_pos])
+            if dialogue_text:
+                dialogues.append({
+                    "text": dialogue_text,
+                    "start": formated_line[start_pos],
+                    "end": formated_line[end_pos]
+                })
 
-            if line.startswith("Format:"):
-                dialogue_format = line[7:].strip().split(', ')
-                start_pos = dialogue_format.index("Start")
-                end_pos = dialogue_format.index("End")
-                text_pos = dialogue_format.index("Text")
+        if line.startswith("Format:"):
+            dialogue_format = line[7:].strip().split(', ')
+            start_pos = dialogue_format.index("Start")
+            end_pos = dialogue_format.index("End")
+            text_pos = dialogue_format.index("Text")
     return dialogues
